2ac138d55812526ccbaa5fa2d8cd2363c2f00566
James Hawker <526rserbinenko@frhsd.com>
2025-02-11 18:48:33 +0000

    added menu animations

--- script.js
+++ script.js
@@ -9,3 +9,56 @@ let quotes = [
   "It takes a great man to be a good listener.",
   "Don’t expect to build up the weak by pulling down the strong.",
 ];
+let ulContent;
+
+window.onload = function startup() {
+  let menu = document.getElementById("mega-menu");
+  let nav = document.getElementById("navbar");
+  menu.setAttribute("style", `top: -${menu.offsetHeight + nav.offsetHeight}px`);
+  let anims = `@keyframes menuUp {
+  from {
+    top: 0;
+  }
+    to {
+    top: -${menu.offsetHeight + nav.offsetHeight}px
+    }
+  }
+  @keyframes menuDown {
+  from {
+  top: -${menu.offsetHeight + nav.offsetHeight}px}
+    to {
+    top: 0;}
+  }  
+  `;
+  let classes = `
+  .menuUp {
+  animation-name: menuUp;
+  animation-duration: 0.6s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in-out;
+  }
+  .menuDown {
+  animation-name: menuDown;
+  animation-duration: 0.6s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in-out;
+  }
+  `;
+  document.getElementsByTagName("STYLE")[0].append(anims);
+  document.getElementsByTagName("STYLE")[0].append(classes);
+};
+
+function menuDown() {
+  document.getElementById("mega-menu").classList.remove("menuUp");
+  document.getElementById("mega-menu").classList.add("menuDown");
+  ulContent = document.getElementById("ul").innerHTML;
+  document.getElementById("ul").innerHTML = "";
+}
+
+function menuUp() {
+  document.getElementById("mega-menu").classList.remove("menuDown");
+  document.getElementById("mega-menu").classList.add("menuUp");
+  document.getElementById("ul").innerHTML = ulContent;
+}
