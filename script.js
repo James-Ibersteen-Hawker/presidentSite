62dbf921b8c3eae92496499bea4810c4fb7dcf6f
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2025-02-11 11:52:16 -0500

    made nonfunctional mega menu

---
+++ script.js
@@ -0,0 +1,11 @@
+let quotes = [
+  "The nation which forgets its defenders will itself be forgotten",
+  "We cannot do everything at once, but we can do something at once",
+  "I have never been hurt by what I have not said.",
+  "Collecting more taxes than is absolutely necessary is legalized robbery.",
+  "No person was ever honored for what he received. Honor has been the reward for what he gave.",
+  "If you see ten troubles coming down the road, you can be sure that nine will run into the ditch before they reach you.",
+  "Nothing in the world can take the place of persistence. Talent will not; nothing is more common than unsuccessful men with talent.",
+  "It takes a great man to be a good listener.",
+  "Don’t expect to build up the weak by pulling down the strong.",
+];
